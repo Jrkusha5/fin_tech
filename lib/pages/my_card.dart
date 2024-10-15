23c07de8560403d6a3a39fec6099ab9334f046cb
Chernet Tafesse <120357658+Jrkusha5@users.noreply.github.com>
2024-10-15 06:33:49 -0700

    commit

--- lib/pages/my_card.dart
+++ lib/pages/my_card.dart
@@ -36,11 +36,82 @@ class MyCardPage extends StatelessWidget {
     );
   }
 }
+
+
 class BackCard extends StatelessWidget {
   const BackCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      height: 240,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: const Color.fromARGB(255, 14, 19, 29)),
+      child: Stack(
+        children: [
+          Positioned(
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: Image.asset(
+              "assets/card-design.png",
+              fit: BoxFit.cover,
+              width: 160,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        CircleAvatar(
+                          radius: 15,
+                          backgroundColor: Colors.white.withOpacity(0.8),
+                        ),
+                        Transform.translate(
+                          offset: const Offset(-10, 0),
+                          child: CircleAvatar(
+                            radius: 15,
+                            backgroundColor: Colors.white.withOpacity(0.8),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "**** **** **** 1990",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(
+                      "9/23",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+                const Text(
+                  "Tesloach Ker",
+                  style: TextStyle(color: Colors.white, fontSize: 17, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
